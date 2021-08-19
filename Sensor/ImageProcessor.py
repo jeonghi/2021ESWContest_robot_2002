4aa74deee942b9bdc1a4a9eb546b9b8d31f62585
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-08-19 22:22:13 +0900

    Modify ImageProcessor 개발환경에 따라 영상소스 바꾸도록, 객체 생성시 video_path 입력 받도록 리팩토링

--- Sensor/ImageProcessor.py
+++ Sensor/ImageProcessor.py
@@ -1,66 +1,47 @@
 import cv2
 import numpy as np
 import time
+import platform
+import os
 from imutils.video import WebcamVideoStream
 from imutils.video import FileVideoStream
 from imutils.video import FPS
 
-
 class ImageProcessor:
-    # cam = WebcamVideoStream(0).start()  # 이미지 공급 쓰레드 인자값 무적권 -1 줘야해 ! ( 라이브 용, 라즈베리파이 캠 용)
-    #video_path = "src/black_area.mp4"
-    #video_path = "src/green_area.mp4"
-    #video_path = "src/alphabet_test1.mp4"
-    #video_path = "src/alphabet_test2.mp4"
-    #video_path = "src/ewsn.mp4"
-    video_path = "src/N.h264"
-    _cam = FileVideoStream(path=video_path).start()
 
-    def __init__(self):
-
-        # ****** 디버깅 영상용 이미지 공급 쓰레드 , 라즈베리파이 캠 키고 하려면 아래 주석처리
-
-        self.fps = FPS()  # 개발때 알고리즘 fps 체크하기 위한 모듈. 실전에서는 필요없음
+    def __init__(self, video_path : str = ""):
+        if video_path and os.path.exists(video_path):
+            self._cam = FileVideoStream(path=video_path).start()
+        else:
+            if platform.system() == "Linux":
+                self._cam = WebcamVideoStream(src=-1).start()
+            else:
+                self._cam = WebcamVideoStream(src=0).start()
+        # 개발때 알고리즘 fps 체크하기 위한 모듈. 실전에서는 필요없음
+        self.fps = FPS()
         shape = (self.height, self.width, _) = self.get_image().shape
         print(shape)  # 이미지 세로, 가로 (행, 열) 정보 출력
-        # time.sleep(2)  # 카메라 포트 여는 시간 반영해서 슬립 조금 줌.
+        time.sleep(2)
 
-    #@staticmethod
     def get_image(self, visualization=False):
-        src = self._cam.read().copy()
+        src = self._cam.read()
+        if src is None:
+            exit()
         if visualization:
             cv2.imshow("Src", src)
             cv2.waitKey(1)
         return src
 
-    def canny(self, visualization=False):
-        from imutils import auto_canny
-        src = self.get_image()
-        dst = auto_canny(src)
-        if visualization:
-            cv2.imshow("Canny", dst)
-            cv2.waitKey(1)
-        return dst
-
-    def feature_matching(self, src):
-        pass
 
 
 
 if __name__ == "__main__":
 
-    imageProcessor = ImageProcessor()
+    imageProcessor = ImageProcessor(video_path="")
     imageProcessor.fps.start()
-    # time.sleep(2)
     #while imageProcessor.fps._numFrames < 200:
     while True:
-        src = imageProcessor.get_image(visualization=False)
-        ret, mask = cv2.threshold(cv2.cvtColor(src,cv2.COLOR_BGR2GRAY), 50, 255, cv2.THRESH_BINARY_INV)
-
-        src = cv2.bitwise_and(src, src, mask=mask)
-        cv2.imshow("src", src)
-        #cv2.imshow("mask", mask)
-        cv2.waitKey(1)
+        src = imageProcessor.get_image(visualization=True)
         imageProcessor.fps.update()
     imageProcessor.fps.stop()
     print("[INFO] time : " + str(imageProcessor.fps.elapsed()))
