e93661a861ce3996b119f70fda0aa29200e7ba26
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-08-20 14:02:55 +0900

    Modify Target 객체 contour 이용해서도 생성할 수 있도록 생성자오버로딩

--- Sensor/Target.py
+++ Sensor/Target.py
@@ -9,8 +9,14 @@ class Target:
         self._center_pos = (self._center_x, self._center_y) = int(centroid[0]), int(centroid[1])
         self._color = color
 
+    def __init__(self, color=None, contour=None):
+        (self._x, self._y, self._width, self._height) = cv2.boundingRect(contour)
+        self._area = cv2.contourArea(contour)
+        self._center_pos = (self._center_x, self._center_y) = (self._x+(self._width//2), self._y+(self._height//2))
+        self._color = color
+
     def get_target_roi(self, src, pad=0, visualization=False) -> np.ndarray:
-        shape = (h,w,_) = src.shape
+        shape = (h,w) = src.shape[:2]
         min_x = self._x - pad
         max_x = self._x + self._width + pad
         min_y = self._y - pad
@@ -24,9 +30,9 @@ class Target:
         roi = src[min_y:max_y, min_x:max_x]
 
         if visualization:
-            dst = cv2.circle(src, self._center_pos, 10, (0, 0, 255), 10)
+            dst = cv2.circle(src, self._center_pos, 10, (255, 255, 255), 10)
             cv2.rectangle(dst, (self._x, self._y), (self._x + self._width, self._y + self._height), (0, 0, 255))
-            if pad > 0: cv2.rectangle(dst, (min_x, min_y), (max_x, max_y), (0, 255, 0))
+            if pad > 0: cv2.rectangle(dst, (min_x, min_y), (max_x, max_y), (255, 255, 255))
             cv2.imshow("target", dst)
             cv2.imshow("target_roi", roi)
             cv2.waitKey(1)
@@ -49,21 +55,29 @@ def setLabel(src, pts, label):
 if __name__ == "__main__":
     from Sensor.ImageProcessor import ImageProcessor
     from imutils import auto_canny
-    imageProcessor = ImageProcessor(video_path='src/N.h264')
+    from Sensor.HashDetector import HashDetector
+    imageProcessor = ImageProcessor(video_path='src/S.h264')
+    hashDetector = HashDetector()
     while True:
+        targets = []
         src = imageProcessor.get_image()
-        src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-        _, mask = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        #_, mask = cv2.threshold(src, 50, 255, cv2.THRESH_BINARY_INV)
-        cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        mask = auto_canny(mask)
+        cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in cnts:
             approx = cv2.approxPolyDP(cnt, cv2.arcLength(cnt, True)*0.02, True)
             vertice = len(approx)
 
             if vertice == 4 and cv2.contourArea(cnt)> 2500:
-                setLabel(src, cnt, 'Rec')
-        cv2.imshow("src", src)
-        cv2.imshow("mask", mask)
-        cv2.waitKey(1)
+                targets.append(Target(contour=cnt))
+        if targets:
+            targets.sort(key= lambda x: x.get_area)
+            roi = targets[0].get_target_roi(src = src, pad=10, visualization=True)
+            print(hashDetector.detect_direction_hash(roi))
+
+        #cv2.imshow("src", src)
+        #cv2.imshow("mask", mask)
+        #cv2.waitKey(1)
 
 
