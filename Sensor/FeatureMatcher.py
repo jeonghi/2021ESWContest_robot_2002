c28761ee059dae2b82b61b2658eb90f7cf68ad73
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-08-20 14:52:45 +0900

    Modify 특징점 매칭 알고리즘 동적 roi 설정후 정확도 테스트 코드 추가

--- Sensor/FeatureMatcher.py
+++ Sensor/FeatureMatcher.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 class FeatureMatcher():
-    def __init__(self, path, mode=1):
+    def __init__(self, path, mode=3):
         self.descripter = self._set_descripter(mode=mode)
         self.path = path
         self.images = []
@@ -15,15 +15,18 @@ class FeatureMatcher():
             img_cur = cv2.imread(f'{self.path}/{cl}',0)
             self.images.append(img_cur)
             self.class_names.append(os.path.splitext(cl)[0])
-        self.descripter_lst = self._find_descripter()
+        self.descripter_lst = self._find_descripter() #  비교기준들에 대한 디스크립터들을 미리 구해놓는다.
+        print(self.class_names)
 
     def _set_descripter(self, mode):
-        if mode == 1:
-            return cv2.AKAZE_create()
+        if mode == 0:
+            return cv2.KAZE_create()
+        elif mode == 1:
+            return cv2.BRISK_create()
         elif mode == 2:
             return cv2.ORB_create()
-        else:
-            return cv2.SIFT_create()
+        elif mode == 3:
+            return cv2.SIFT_create() # parameter : contrastThreshold
 
     def _find_descripter(self):
         tmp = []
@@ -42,7 +45,7 @@ class FeatureMatcher():
                 matches = bf.knnMatch(des1,des2,k=2)
                 good = []
                 for m, n in matches:
-                    if m.distance < 0.75 * n.distance:
+                    if m.distance < 0.65 * n.distance:
                         good.append([m])
                 match_lst.append(len(good))
         except:
@@ -60,16 +63,40 @@ class FeatureMatcher():
         else: return None
 
 if __name__ == "__main__":
-    from ImageProcessor import ImageProcessor
-    image_processor = ImageProcessor()
-    feature_matcher1 = FeatureMatcher(path="RoomName", mode=3)
+    from Sensor.ImageProcessor import ImageProcessor
+    from Sensor.Target import Target
+    from imutils import auto_canny
+    imageProcessor = ImageProcessor(video_path="src/S.h264")
+    feature_matcher1 = FeatureMatcher(path="EWSN", mode=3)
     while True:
-        src1 = image_processor.get_image()
-        class_name1 = feature_matcher1.get_class_name(cv2.cvtColor(cv2.resize(src1,(320,240)), cv2.COLOR_BGR2GRAY))
-        if class_name1 != -1 and class_name1 != None:
-            src1 = cv2.putText(src1, f"Detected: {class_name1}", (0, 80), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 3)
-        else:
-            src1 = cv2.putText(src1, f"Detected: None", (0, 80), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 3)
+        targets = []
+        src = imageProcessor.get_image()
+        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        mask = auto_canny(mask)
+        cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in cnts:
+            approx = cv2.approxPolyDP(cnt, cv2.arcLength(cnt, True) * 0.02, True)
+            vertice = len(approx)
 
-        cv2.imshow('img', src1)
+            if vertice == 4 and cv2.contourArea(cnt) > 2500:
+                targets.append(Target(contour=cnt))
+        if not targets :
+            continue
+        targets.sort(key=lambda x: x.get_area)
+        roi = targets[0].get_target_roi(src=src, pad=10, visualization=True)
+        roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(roi_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        #mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        cv2.imshow("roi thresh", mask)
+        cv2.waitKey(1)
+
+        class_name1 = feature_matcher1.get_class_name(mask)
+        #class_name1 = feature_matcher1.get_class_name(cv2.cvtColor(cv2.resize(src1,(320,240)), cv2.COLOR_BGR2GRAY))
+        if class_name1 != -1 and class_name1 != None:
+            src1 = cv2.putText(src, f"Detected: {class_name1[0]}", (0, 80), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 3)
+        else:
+            src1 = cv2.putText(src, f"Detected: None", (0, 80), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 3)
+            pass
+        cv2.imshow('img', src)
         cv2.waitKey(1)
