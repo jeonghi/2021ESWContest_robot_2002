bedcc6fc51d69447e1dce0107d335f0642f36938
mrgentle1 <eunjini0719@naver.com>
2021-09-02 21:20:39 +0900

    Add line tracing function

--- Brain/Controller.py
+++ Brain/Controller.py
@@ -1,4 +1,5 @@
 from Sensor.ImageProcessor import ImageProcessor
+from Sensor.lines_class import LineDetector
 from Actuator.Motion import Motion
 import numpy as np
 import cv2
@@ -19,3 +20,27 @@ class Robot:
         alphabet = self._image_processor.get_door_alphabet()
         self._motion.notice_direction(dir=alphabet)
         exit()
+
+    def line_tracing(self):
+        src = self._image_processor.get_image(visualization=False)
+        src = cv2.resize(src, dsize=(480, 640))
+        ans, src = LineDetector.get_all_lines(src)
+        # ans list : [0]현재 방향(동작 보정 각도), [1]vertical, [2]vertical-x, [3]horizontal, [4]horizontal-y, [5]horizontal-minx, [6]horizontal-max
+        if ans[1] != 'None' and ans[3] == 'None': #직진
+            if 80< ans[0]<100: # 각도가 80~100 이도록
+                self._motion.walk(dir='FORWARD', loop=1) # 일정 y좌표때 멈추기
+        elif ans[1] != 'None' and ans[3] != 'None':
+            # ans[5] 수평선의 마지막 x좌표 - 중앙보다 작으면 ㄱ , 중앙보다 크면 T 
+            if 200 < ans[4]:
+                if ans[5] == 0 and ans[6] < 340:
+                    # ㄱ자
+                    print('ㄱ자')
+                elif ans[5] > 300 and ans[6] == 640:
+                    # ㄴ자
+                    print('ㄴ자')
+                else:
+                    # T자
+                    print('T자')
+            else:
+                self._motion.walk(dir='FORWARD')
+        
