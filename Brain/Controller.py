1c606fe969125dba7e36e342ebbeddfb8aca279c
mrgentle1 <eunjini0719@naver.com>
2021-09-05 16:15:08 +0900

    fix

--- Brain/Controller.py
+++ Brain/Controller.py
@@ -31,21 +31,14 @@ class Robot:
             cv2.waitKey(1)
             # ans list : [0]현재 방향(동작 보정 각도), [1]vertical, [2]vertical-x, [3]horizontal, [4]horizontal-y, [5]horizontal-minx, [6]horizontal-max
             if ans[1] is not None and ans[3] is None: #직진
-                if 80< ans[0]<100: # 각도가 80~100 이도록
-                    print('FORWARD', ans[0])
-                    self._motion.walk(dir='FORWARD', loop=1) # 일정 y좌표때 멈추기
-                elif ans[0] < 80:
-                    print('RIGHT', ans[0])
-                    self._motion.turn(dir='RIGHT', loop=1)
-                elif ans[0] > 100:
-                    print('LEFT', ans[0])
-                    self._motion.turn(dir='LEFT', loop=1)
+                print('FORWARD', ans)
+                self._motion.walk(dir='FORWARD', loop=1) # 일정 y좌표때 멈추기
             elif ans[1] is not None and ans[3] is not None:
                 # ans[5] 수평선의 마지막 x좌표 - 중앙보다 작으면 ㄱ , 중앙보다 크면 T 
                 if 200 < ans[4]:
                     if ans[5] < 50 and ans[6] < 340:
                         # ㄱ자
-                        print('ㄱ자', ans[0])
+                        print('ㄱ자', ans)
                         self._motion.turn('LEFT', loop=4)
                     elif ans[5] > 300 and ans[6]>600:
                         # ㄴ자
@@ -53,8 +46,24 @@ class Robot:
                         self._motion.turn('RIGHT', loop=4)
                     else:
                         # T자
-                        print('T자', ans[0])
+                        print('T자', ans)
                         return 0
                 else:
-                    print('FORWARD', ans[0])
+                    print('FORWARD', ans)
                     self._motion.walk(dir='FORWARD')
+
+
+            elif ans[1] is None and ans[3] is None:
+                if ans[0] < 80:
+                    print('RIGHT', ans)
+                    self._motion.turn(dir='RIGHT', loop=1)
+                elif ans[0] > 100:
+                    print('LEFT', ans)
+                    self._motion.turn(dir='LEFT', loop=1)
+
+
+            elif ans[1] is None and ans[3] is not None:
+                print("ans[1] is None and ans[3] is not None")
+                print(ans)
+            else:
+                print("else")
