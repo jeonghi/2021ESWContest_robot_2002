4ee26aa84e3eeb24281293cd854c8b331b6f7ccd
mrgentle1 <eunjini0719@naver.com>
2021-09-05 19:41:11 +0900

    fix

--- Brain/Controller.py
+++ Brain/Controller.py
@@ -85,45 +85,50 @@ class Robot:
             elif ans[1] is None and ans[3] is not None: # 수평만 검출, ㄱ자랑 T자 앞에 있다는 뜻
                 # 가려는 방향으로 두칸 이동 및 회전
                 #print('INFRONT OF LINE --go LEFT 2 and trun LEFT', ans)
-
-                if ans[5] < 50 and ans[6] < 340:
-                    # ㄱ자
-                    print('ㄱ자', ans)
-                    self._motion.turn('LEFT', loop=8)
-                elif ans[5] > 300 and ans[6]>600:
-                    # ㄴ자
-                    print('ㄴ자', ans)
-                    self._motion.turn('RIGHT', loop=8)
+                if ans[4] > 200: 
+                    if ans[5] < 50 and ans[6] < 340:
+                        # ㄱ자
+                        print('ㄱ자', ans)
+                        self._motion.turn('LEFT', loop=8)
+                    elif ans[5] > 300 and ans[6]>600:
+                        # ㄴ자
+                        print('ㄴ자', ans)
+                        self._motion.turn('RIGHT', loop=8)
+                    else:
+                        # T자 :: 가려는 방향으로 두칸 이동 및 회전
+                        print('T자 :: INFRONT OF LINE --go LEFT 2 and trun LEFT', ans)
+                        self._motion.walk('LEFT', 4)
+                        self._motion.turn('LEFT', 8)
                 else:
-                    # T자 :: 가려는 방향으로 두칸 이동 및 회전
-                    print('T자 :: INFRONT OF LINE --go LEFT 2 and trun LEFT', ans)
-                    self._motion.walk('LEFT', 4)
-                    self._motion.turn('LEFT', 8)
+                    print(ans[4], 'low then 200')
 
             elif ans[1] is not None and ans[3] is not None: # 수직 수평 둘다 검출
                 # ans[5] 수평선의 마지막 x좌표 - 중앙보다 작으면 ㄱ , 중앙보다 크면 T 
-                if ans[5] < 50 and ans[6] < 340:
-                    # ㄱ자
-                    print('ㄱ자', ans)
-                    self._motion.turn('LEFT', loop=8)
-                elif ans[5] > 300 and ans[6]>600:
-                    # ㄴ자
-                    print('ㄴ자', ans)
-                    self._motion.turn('RIGHT', loop=8)
+                if ans[4] > 200: 
+                    if ans[5] < 50 and ans[6] < 340:
+                        # ㄱ자
+                        print('ㄱ자', ans)
+                        self._motion.turn('LEFT', loop=8)
+                    elif ans[5] > 300 and ans[6]>600:
+                        # ㄴ자
+                        print('ㄴ자', ans)
+                        self._motion.turn('RIGHT', loop=8)
+                    else:
+                        # T자 :: 가려는 방향으로 두칸 이동 및 회전
+                        print('T자 :: INFRONT OF LINE --go LEFT 2 and trun LEFT', ans)
+                        self._motion.walk('LEFT', 4)
+                        self._motion.turn('LEFT', 8)
                 else:
-                    # T자 :: 가려는 방향으로 두칸 이동 및 회전
-                    print('T자 :: INFRONT OF LINE --go LEFT 2 and trun LEFT', ans)
-                    self._motion.walk('LEFT', 4)
-                    self._motion.turn('LEFT', 8)
+                    print(ans[4], 'low then 200')
 
 
             elif ans[1] is None and ans[3] is None: # 아무 직선도 검출 안됨(수직, 수평선 검출될 때까지 계속 회전)
                 if ans[0] < 80:
-                    print('MODIFY angle --LEFT', ans)
-                    self._motion.turn(dir='LEFT', loop=1)
-                elif ans[0] > 100:
                     print('MODIFY angle --RIGHT', ans)
                     self._motion.turn(dir='RIGHT', loop=1)
+                elif ans[0] > 100:
+                    print('MODIFY angle --LEFT', ans)
+                    self._motion.turn(dir='LEFT', loop=1)
 
             else:
                 print("else")
