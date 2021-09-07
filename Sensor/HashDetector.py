982b747f99488aab3aa0c98db8e7635cf318ceae
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-09-07 13:06:14 +0900

    Modify function name detect_direction_hash to detect_alphabet_hash for purpose of method detecting alphabet Add function parameter for setting threshold value

--- Sensor/HashDetector.py
+++ Sensor/HashDetector.py
@@ -8,7 +8,10 @@ class HashDetector:
         self.directions_hash = []
         self.directions = []
         self.check_file_type(file_path)
-        for direction in os.listdir(file_path):
+        self.file_lst = os.listdir(file_path)
+        if '.DS_Store' in self.file_lst:
+            self.file_lst.remove('.DS_Store')
+        for direction in self.file_lst:
             self.directions_hash.append(self.image_to_hash(cv2.imread(file_path + direction)))
             self.directions.append(direction.rsplit('.')[0])
         print(file_path + direction)
@@ -46,7 +49,7 @@ class HashDetector:
         assert no_files_in_folder == no_files_allowed, "The extension in the folder should all be the same, but found more than one extensions"
         return extension_type
 
-    def detect_direction_hash(self, img : np.ndarray) -> str:
+    def detect_alphabet_hash(self, img : np.ndarray, threshold=0.3) -> str:
         img_hash = self.image_to_hash(img)
         hdist_dict = {}
         
@@ -57,7 +60,7 @@ class HashDetector:
         
         result = min(hdist_dict.keys(), key=(lambda k:hdist_dict[k]))
         
-        if hdist_dict[result] > 0.1:
+        if hdist_dict[result] > threshold:
             return None
         
         return result
@@ -67,8 +70,8 @@ if __name__ == "__main__":
     from Sensor.ImageProcessor import ImageProcessor
     from imutils import auto_canny
     from Sensor.Target import Target
-    imageProcessor = ImageProcessor(video_path='src/N.h264')
-    hashDetector = HashDetector()
+    imageProcessor = ImageProcessor(video_path='src/S.h264')
+    hashDetector = HashDetector(file_path='EWSN/')
     while True:
         targets = []
         src = imageProcessor.get_image()
@@ -84,10 +87,10 @@ if __name__ == "__main__":
                 targets.append(Target(contour=cnt))
         if targets:
             targets.sort(key= lambda x: x.get_area)
-            roi = targets[0].get_target_roi(src = src, pad=10, visualization=True)
+            roi = targets[0].get_target_roi(src = src, pad=0, visualization=True)
             roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-            _, mask = cv2.threshold(roi_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            _, mask = cv2.threshold(roi_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
             mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
             cv2.imshow("roi thresh", mask)
             cv2.waitKey(1)
-            print(hashDetector.detect_direction_hash(roi))
+            print(hashDetector.detect_alphabet_hash(mask))
