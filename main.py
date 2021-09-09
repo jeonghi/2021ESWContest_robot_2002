08833db589b1c3b64b263f3ff96f36f0d4ed493e
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-09-09 21:27:36 +0900

    Modify 미션 구역 확인 코드

--- main.py
+++ main.py
@@ -7,21 +7,7 @@ import cv2
 
 def main():
     robot = Robot()
-    #robot.detect_alphabet()
-    robot.line_tracing()
-    
-
-def test():
-    imageProcessor = ImageProcessor(video_path="Sensor/src/ewsn.mp4")
-    imageProcessor.fps.start()
-    #while imageProcessor.fps._numFrames < 200:
-    while True:
-        src = imageProcessor.get_image(visualization=True)
-        print(imageProcessor.get_door_alphabet(visualization=True))
-        imageProcessor.fps.update()
-    imageProcessor.fps.stop()
-    print("[INFO] time : " + str(imageProcessor.fps.elapsed()))
-    print("[INFO] FPS : " + str(imageProcessor.fps.fps()))
+    robot.recognize_area_color()
 
 if __name__ == "__main__":
     main()
