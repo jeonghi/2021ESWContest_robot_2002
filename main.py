c982fbd45b343ef0ebd198bc54bd50a86e2babdc
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2021-09-18 02:46:45 +0900

    add debug in main

--- main.py
+++ main.py
@@ -3,12 +3,38 @@ from imutils import video
 from Sensor.LineDetector import LineDetector
 from Brain.Controller import Robot
 from Sensor.ImageProcessor import ImageProcessor
+from Sensor.VideoRecorder import VideoRecorder
+
 import cv2
+import kbhit # press any key to exit
 
 def main():
     robot = Robot()
+    #====================== debug ======================
+
+    video_recorder = VideoRecorder()
+    kb = kbhit.KBHit()
+
+    print("Press ESC key to exit")
     while True:
-        robot.setting_mode_test()
+        if kb.kbhit():
+            key = ord(kb.getch())
+
+            if key == 27: # ESC
+                break
+
+        frame = robot._image_processor.get_image()
+        video_recorder.record_frame(frame)
+
+    #===================== function ======================
+
+        robot.tracking_cube()
+
+
+    #=====================================================
+    
+    video_recorder.stop()
+    robot.set_basic_form()
 
 if __name__ == "__main__":
     main()
