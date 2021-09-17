0bb42b3824dafc94f671cabfca46a47a18e99ded
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2021-09-18 03:59:00 +0900

    delete time.sleep

--- RemoteController.py
+++ RemoteController.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         command = input("Input command: ")
         command_key = None
 
-        if command == "power on" or "power off":
+        if command == "power on" or command == "power off":
             command_key = 16
         elif command == "head vcenter":
             command_key = 44
@@ -18,7 +18,8 @@ if __name__ == "__main__":
             command_key = 64
         elif command == "grab off":
             command_key = 65
+        elif command == "exit":
+            break
         
         print("Launch " + command)
         motion.TX_data_py2(command_key)
-        time.sleep(0.5)
