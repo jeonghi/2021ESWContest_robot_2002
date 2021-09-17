d9f15b89c7b090e81ca056375cef9dfd19bb8464
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2021-09-18 05:20:51 +0900

    오타 수정

--- Sensor/VideoRecorder.py
+++ Sensor/VideoRecorder.py
@@ -4,7 +4,7 @@ import numpy as np
 class VideoRecorder:
     def __init__(self):
         fourcc = cv2.VideoWriter_fourcc(*"DIVX")
-        self.video_recorder = cv2.VideoWriter('debug_vedio.avi', fourcc, 30.0, (640, 480))
+        self.video_recorder = cv2.VideoWriter('debug_video.avi', fourcc, 30.0, (640, 480))
 
     def record_frame(self, frame):
         self.video_recorder.write(frame)
