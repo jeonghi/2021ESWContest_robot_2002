3ad72d664652655a9b1797697ea8447b82a51519
solahn <dksthf0102@gmail.com>
2021-10-03 16:53:08 +0900

    old line tracing

--- Brain/Controller.py
+++ Brain/Controller.py
@@ -50,6 +50,75 @@ class Robot:
         #self._motion.move_arm(dir = 'LOW')
         self._motion.set_head(dir='DOWN', angle = 20)
 
+
+    def line_tracing(self):
+        self._motion.set_head('DOWN', 30)
+        while True:
+            src = self._image_processor.get_image(visualization=False)
+            src = cv2.resize(src, dsize=(640,480))
+            line_info, result_img = self._line_detector.get_all_lines(src)
+            cv2.imshow('src', result_img)
+            cv2.waitKey(1)
+            if line_info["V"] and not line_info["H"]:
+                if 300 < line_info["VPOS"] <340:
+                    print('FORWARD', line_info)
+                    self._motion.walk(dir='FORWARD', loop=1)
+                else:
+                    if line_info["VPOS"] < 300:
+                        print('MODIFY walk --LEFT', line_info)
+                        self._motion.walk(dir='LEFT', loop=1)
+                    elif line_info["VPOS"] > 340:
+                        print('MODIFY walk --RIGHT', line_info)
+                        self._motion.walk(dir='RIGHT', loop=1)
+            elif not line_info["V"] and not line_info["H"]:
+                if line_info["DEGREE"] < 85:
+                    print('MODIFY angle --LEFT', line_info)
+                    self._motion.turn(dir='LEFT', loop=1)
+                elif line_info["DEGREE"] > 95:
+                    print('MODIFY angle --RIGHT', line_info)
+                    self._motion.turn(dir='RIGHT', loop=1)
+            else:
+                if line_info["HPOS"] > 200:
+                    if line_info["H_MIN_X"] < 50 and line_info["H_MAX_X"] < 340:
+                        print('┒', line_info)
+                        if self.direction == 'LEFT':
+                            print(self.direction, "모드여서 좌회전 안하고 직진합니다.")
+                            self._motion.walk(dir='FORWARD', loop=4)
+                            #self.count += 1
+                        elif self.direction == 'RIGHT':
+                            self._motion.turn('LEFT', loop=8)
+                        else:
+                            print("화살표 인식 전입니다.")
+                    elif line_info["H_MIN_X"] > 300 and line_info["H_MAX_X"]>600:
+                        print('┎', line_info)
+                        if self.direction == 'LEFT':
+                            self._motion.turn('RIGHT', loop=8)
+                        elif self.direction == 'RIGHT':
+                            print(self.direction, "모드여서 우회전 안하고 직진합니다.")
+                            self._motion.walk(dir='FORWARD', loop=4)
+                            #self.count += 1
+                        else:
+                            print("화살표 인식 전입니다.")
+                    else:
+                        print('T', line_info)
+                        self._motion.set_head("DOWN", 100)
+                        self.direction = self._image_processor.get_arrow_direction()
+                        print('MODE::', self.direction)
+                        time.sleep(0.1)
+                        while self.direction == None:
+                            self._motion.walk("BACKWARD", 1)
+                            self.direction = self._image_processor.get_arrow_direction()
+                            print('MODE::', self.direction)
+                            time.sleep(0.1)
+                        self._motion.set_head('DOWN', 10)
+                        self._motion.walk('FORWARD', 2)
+                        self._motion.walk(self.direction, 4)
+                        self._motion.turn(self.direction, 8)
+                else:
+                    self._motion.walk('FORWARD')
+                    print('low then 150')
+
+
     def detect_alphabet(self):
         self._motion.set_head('DOWN', 75)
         flag = True
