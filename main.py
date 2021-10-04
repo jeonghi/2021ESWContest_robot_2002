bca9d15ffb7a9443367f1d2ea4d72fdd77df7d17
lovelyoverflow <lovelyoverflow@gmail.com>
2021-10-04 14:07:07 +0900

    restore kbhit

--- main.py
+++ main.py
@@ -6,37 +6,38 @@ from Sensor.ImageProcessor import ImageProcessor
 from Sensor.VideoRecorder import VideoRecorder
 
 import cv2
-#import kbhit # press any key to exit
+import kbhit # press any key to exit
 
 def main():
-    robot = Robot()
+    #robot = Robot()
+    image_processor = ImageProcessor("Sensor/src/door_test/doortest_E.h264")
     #====================== debug ======================
 
     video_recorder = VideoRecorder()
-    #kb = kbhit.KBHit()
+    kb = kbhit.KBHit()
 
     print("Press ESC key to exit")
     while True:
-        # if kb.kbhit():
-        #     key = ord(kb.getch())
-        #
-        #     if key == 27: # ESC
-        #         break
+        if kb.kbhit():
+            key = ord(kb.getch())
+        
+            if key == 27: # ESC
+                break
 
-        frame = robot._image_processor.get_image()
-        video_recorder.record_frame(frame)
+        #frame = robot._image_processor.get_image()
+        #video_recorder.record_frame(frame)
 
     #===================== function ======================
 
-        robot.tracking_cube()
+        #robot.tracking_cube()
         #robot.find_yellow_corner_for_out_room()
-
+        print(image_processor.get_door_alphabet(visualization=True))
 
 
     #=====================================================
 
     #video_recorder.stop()
-    robot.set_basic_form()
+    #robot.set_basic_form()
 
 if __name__ == "__main__":
     main()
