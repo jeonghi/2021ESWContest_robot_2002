43d2313f632d258734eab73a1a40f655f3290af5
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2021-10-07 22:00:33 +0900

    remove one move_arm

--- Actuator/Motion.py
+++ Actuator/Motion.py
@@ -213,19 +213,6 @@ class Motion:
         self.TX_data_py2(54)
         self.TX_data_py2(10)
 
-<<<<<<< HEAD
-    def move_arm(self, grab=True, level=2):
-        """level: if grab is True, {1,2,3} else grab is False, {1,2}
-        """
-        angle_list = [30, 90, 60]
-        if grab:
-            self.TX_data_py2(75+level)
-            time.sleep(0.5)
-            self.set_head(dir='DOWN', angle=angle_list[level-1])
-        else:
-            self.TX_data_py2(73+level)
-            time.sleep(0.5)
-            self.set_head(dir='DOWN', angle=100)
 
 =======
     def basic_form(self):
