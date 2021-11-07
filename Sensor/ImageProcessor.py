9e2997254fd031ead76ca429755da0d34280b083
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-11-07 16:51:07 +0900

    refactoring 중

--- Sensor/ImageProcessor.py
+++ Sensor/ImageProcessor.py
@@ -28,8 +28,8 @@ class ImageProcessor:
     _instance = None
 
     def __new__(cls, *args, **kwargs):
-        if not hasattr(cls._instance, "_instance"):
-            cls._instance = super().__new__(cls, *args, **kwargs)
+        if not hasattr(cls, "_instance"):
+            cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self, video_path : str = ""):
