8619d4dc779a6b7cff2ca39455500dd844bc0293
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-11-14 19:15:50 +0900

    수치조정

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Brain.Controller import Controller, Mode
 
 def main():
-    #Controller.set_test_mode(Mode.CHECK_AREA_COLOR)
+    Controller.set_test_mode(Mode.DETECT_DIRECTION)
     
     while not Controller.run():
         continue
