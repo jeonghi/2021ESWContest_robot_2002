a2daa020bfff3163cbe495ad90c85412b4f5e1e4
Park Jeong Hwan <50570113+jeonghi@users.noreply.github.com>
2021-11-18 19:43:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-import os
-os.chdir("/home/pi/2021ESWContest_robot_2002/")
-
 from Brain.Controller import Controller, Mode
 
 def main():
-    #Controller.set_test_mode(Mode.CHECK_AREA_COLOR)
     while not Controller.run():
         continue
 
 if __name__ == "__main__":
     main()
-    #while True:
-        #line, edge, _ = Controller.robot._image_processor.line_tracing(color = 'YELLOW', edge_visualization = True, ROI_edge=True)
-        #print(edge["EDGE_POS"][0] + 200, edge["EDGE_POS"][1])
-        #print(line)
-        #print(Controller.robot._image_processor.is_out_of_black(visualization=True))
 
